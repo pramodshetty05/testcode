ca8ef41e5815c99d1738f5f72082047296dba337
prshetty05 <pramodshetty05@gmail.com>
2022-08-13 00:39:12 +0530

    junky

--- user1.py
+++ user1.py
@@ -1,2 +1,20 @@
 # file created by user1
 # git
+from this import d
+from tkinter.tix import DirSelectDialog
+
+
+# ;lkas;dklasd
+# asd
+# asd
+# a
+# sdistsd
+# ALERT_DESCRIPTION_BAD_CERTIFICATE_STATUS_RESPONSEas
+# DirSelectDialogsda
+# sd
+# asdictasd
+# asd
+# asd
+# a
+# sd
+# as
